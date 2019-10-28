03b4f155b1b0b87e798a8741f143d82bb71ffecf
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-28 15:36:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Add your functions here
-function map(array,function(a){return a*-1}){
-  return array
+function map(array){
+ result=[]
+  for (let i = 0; i<array.length; i++){
+    result[i]=array[i]*-1
+  }
+  return result
 }
